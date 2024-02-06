8ee06a4a9cc058be0082084e7ee0b7fa459d56c3
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-02-06 20:08:03 +0530

    some modifications are made

--- PasswordGenerator/password.py
+++ PasswordGenerator/password.py
@@ -1,4 +1,4 @@
-import string 
+import string
 import random
 
 if __name__ == "__main__":
@@ -6,13 +6,28 @@ if __name__ == "__main__":
     s2 = string.ascii_uppercase
     s3 = string.digits
     s4 = string.punctuation
+
     passLen = int(input("Enter password length\n"))
-    hint = eval(input("Enter any customisation you want name/number :"))
+    customization = input("Enter any customization you want (name/number): ")
+
     s = []
     s.extend(s1)
     s.extend(s2)
     s.extend(s3)
     s.extend(s4)
+    
     random.shuffle(s)
-    print("Your password is :")
-    print("".join(s[0:passLen]))
+    
+    # Generate a password without customization
+    password = "".join(s[0:passLen])
+    
+    if customization.lower() == 'name':
+        # Allow the user to input their name
+        name_input = input("Enter your name: ")
+        password = name_input + password
+    elif customization.lower() == 'number':
+        # Allow the user to input a number
+        number_input = input("Enter a number: ")
+        password = number_input + password
+    
+    print("Your password is:", password)
