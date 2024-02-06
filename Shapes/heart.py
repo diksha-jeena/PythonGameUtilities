4c76cd0a439dcf8c608f564f3c803d1ca636d98b
diksha-jeena <158588723+diksha-jeena@users.noreply.github.com>
2024-02-06 20:14:12 +0530

    simple heart

---
+++ Shapes/heart.py
@@ -0,0 +1,18 @@
+from turtle import *
+color('blue')
+begin_fill()
+pensize(5)
+setposition(0,0)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+penup()
+setposition(0,200)
+pendown()
+
+
+
